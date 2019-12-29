7e64ff39bf4ab80b1afdc8ec6c480b3dc5b23040
Ethan Irvine <edog.irvine@gmail.com>
2019-12-29 10:31:49 -0700

    Decided to delete weather.html and just have one page.

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 $(document).ready(function(){
-    weatherData();
+    basicWeatherData();
+   
 });
-function weatherData(){
-    const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Boston&APPID=9be0a529a7dd200677c71e4ba94edd63&units=imperial";
+// saves the search into our console
+  $('#search').click(function(){
+    const citySearched = $(this).siblings('input').val().toUpperCase();
+        localStorage.setItem('city', citySearched); 
+        $('.mainArea').hide();
+        $('.weatherDisplay').show();
+  });
+
+//   displays our basic information
+function basicWeatherData(){
+    const city = localStorage.getItem('city');
+    const queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&APPID=9be0a529a7dd200677c71e4ba94edd63&units=imperial";
     
     $.ajax({
       url: queryURL,
       method: "GET",
     }).then(function(response) {
-
+        const weatherTemp = response.main.temp;
+        const weatherHumidity = response.main.humidity;
+        const windSpeed = response.wind.speed;
+        
+        $('.weatherDisplay').append("<h1>"+ city +"</h1>");
+        $('.weatherDisplay').append("<p>"+ weatherTemp +" Degrees </p>");
+        $('.weatherDisplay').append("<p>"+ weatherHumidity +"% Humid </p>");
+        $('.weatherDisplay').append("<p>"+ windSpeed +" MPH </p>");
       // Printing the entire object to console
       console.log(response);
     });
   }
+
+//   $('.card').append("<p>"+  +" </p>");
